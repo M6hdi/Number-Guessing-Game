7780bc29b4807da0d3d992f348e596e2599a9f72
MN16 <mn16mn16mn16@gmail.com>
2022-09-05 15:22:33 +0430

    update

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let RandomNum = Math.floor(Math.random()*20);
+let RandomNum = Math.floor(Math.random()*10);
 document.querySelector("#buttonarea").addEventListener("click",clickguess);
-function clickguess() {
+function clickguess(){
     let userguess = Number(document.querySelector("#userguess").value);
-    if(userguess < 1 || userguess >20){
-        alert("enter a number between 1-20!");
+    if(userguess < 1 || userguess >10){
+        alert("enter a number between 1-10!");
     }
     let report = document.querySelector('#report');
-    if(userguess > RandomNum){
-        report.innerHTML = "Sorry! you lose! your guess was up!";
-    }
-    else if(userguess < RandomNum){
-        report.innerHTML = "Sorry! you lose! your guess was low!";
+    if(userguess == RandomNum){
+        report.innerHTML = "Congradulation! You Won!";
+        report.style.color="rgb(10,150,40)";
+        userguess.disabled=true;
+        document.querySelector("#restart").style.display="inline-block";
     }
     else{
-        report.innerHTML = "you won!";
-        alert("Congradulation! You win!");
+        report.innerHTML = "Sorry! You Lost!";
+        report.style.color="rgb(230,20,10)";
+        document.querySelector('#userguess').disabled=true;
+        document.querySelector("#restart").style.display="inline-block";
     }
 }
-
+document.querySelector("#restart").addEventListener("click",refresh);
+function refresh(){
+    window.location.reload();
+}
